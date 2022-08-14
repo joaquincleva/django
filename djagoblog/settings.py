639884776db14b2dca8bc68755fc01ddfe536928
joaquincleva <joaquin.cleva@gmail.com>
2022-08-14 16:10:55 -0300

    2

--- djagoblog/settings.py
+++ djagoblog/settings.py
@@ -10,6 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
